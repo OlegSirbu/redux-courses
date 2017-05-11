337a5c5d9e42e5835af16055d530b9922490aa30
Oleg Syrbu <olegsirbu2@gmail.com>
2017-05-11 14:39:59 +0300

    init files

---
+++ src/components/course/CoursePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class CoursePage extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Courses page</h1>
+            </div>
+        );
+    }
+}
+
+export default CoursePage;
