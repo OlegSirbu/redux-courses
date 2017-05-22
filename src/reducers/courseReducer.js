9d9ab28bc0aedaaf40e80eb045a5c52a74bd7afc
Oleg Syrbu <olegsirbu2@gmail.com>
2017-05-22 21:09:56 +0300

    implement course actions and reducers

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,23 @@
 import * as types from "../actions/actionsTypes";
+import initialSate from './initialState';
 
-export default function courseReducer(state = [], actions){
+export default function courseReducer(state = initialSate.courses, actions){
   switch(actions.type) {
       case types.LOAD_COURSES_SUCCESS:
         return actions.courses;
 
-      default:
+      case types.CREATE_COURSE_SUCCESS:
+          return [
+            ...state,
+            Object.assign({}, actions.course)
+          ];
+    case types.UPDATE_COURSE_SUCCESS:
+          return [
+            ...state.filter(course => course.id !== actions.course.id),
+            Object.assign({}, actions.course)
+          ];
+
+    default:
         return state
   }
 }
