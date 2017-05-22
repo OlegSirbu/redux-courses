a9b0b621fb9a0ee2041e54cdae5a44ba32b5aadf
Oleg Syrbu <olegsirbu2@gmail.com>
2017-05-22 21:12:35 +0300

    implement select component

---
+++ src/components/common/SelectInput.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+
+const SelectInput = ({name, label, onChange, defaultOptions, value, error, options}) => {
+    let wrapperClasses = 'form-group';
+    if(error && error.length > 0){
+        wrapperClasses += 'has-error';
+    }
+    return(
+        <div className={wrapperClasses} >
+            <label htmlFor={name}>{label}</label>
+            <div className='field'>
+                <select 
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    className='form-group'
+                >
+                <options value="">{defaultOptions}</options>
+                {options.map((option)=> {
+                    return <option key={option.value} value={option.value}>{option.text}</option>
+                })
+                }
+                </select>
+                {error && <div className="alert alert-danger">{error}</div> }
+            </div>
+        </div>
+    );
+};
+
+export default SelectInput;
